78207f7e7ae016c301780fe78c70916a25100824
saadbavo <[saadhmk13@gmail.com]>
2025-04-23 19:32:08 +0100

    Add the fonction for adding book to the arry

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const myLibrary = [];
 
-function Book(title, pages, author) {
+function Book(title, pages, author, yearPublished) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.id = crypto.randomUUID();
+  this.yearPublished = yearPublished;
   if(!new.target){
     throw Error("you must use 'new'");
   }
 }
 
 function addBookToLibrary() {
-  // take params, create a book then store it in the array
+  const newBook = new Book(title, pages, author, yearPublished);
+  myLibrary.push(newBook);
 }
